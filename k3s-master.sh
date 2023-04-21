4097f53f9ed09651128a63d538650d06dfc9624e
Marc Ende <me@e-beyond.de>
2023-04-21 12:49:27 +0200

    Fixx

--- k3s-master.sh
+++ k3s-master.sh
@@ -48,7 +48,7 @@ then
 			--flannel-iface=ens10
 else 
   curl -sfL https://get.k3s.io | K3S_TOKEN=$K3S_TOKEN sh -s - server \
-      --server https://$MASTER_NODE:6443 \
-			--cluster-cidr=10.244.0.0/16 \
+      --server "https://$MASTER_NODE:6443" \
+			--cluster-cidr="10.244.0.0/16" \
       --disable-cloud-controller
 fi
